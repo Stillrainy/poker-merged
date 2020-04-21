02f351e5c3a67f9b433652352a5376ec07e8c0d8
Shuyuan <sy30@students.uwf.edu>
2020-04-21 00:25:16 -0500

    first commit

---
+++ poker.py
@@ -0,0 +1,56 @@
+from PIL import Image
+import os
+import random
+
+
+class Poker():
+
+    def loadFilesName(self):
+        self.filesName = os.listdir('img/')
+        self.length = len(self.filesName)
+
+        for i in range(self.length):
+            self.filesName[i] = 'img/' + self.filesName[i]
+        return self.filesName
+
+    def generateImg(self, width, height, loop):
+        width = int(width * 94.488188976)
+        height = int(height * 94.488188976)
+        image = Image.new('RGBA', (width, height))
+
+        try:
+            from tqdm import tqdm
+            with tqdm(total=loop, ncols=75) as pbar:
+                for i in range(loop):
+                    curImge = Image.open(
+                        self.filesName[random.randint(0, self.length-1)]
+                    )
+
+                    rotImge = curImge.rotate(
+                        random.randint(0, 360),
+                        Image.NEAREST,
+                        expand=1
+                    )
+
+                    image.paste(
+                        rotImge,
+                        (random.randint(-500, width),
+                         random.randint(-500, height)),
+                        rotImge
+                    )
+
+                    pbar.update(1)
+        except KeyboardInterrupt:
+            pbar.close()
+            raise
+        pbar.close()
+
+        print('Generating...')
+        image.save('output/merged.png', dpi=(240, 240))
+        print(r"Output to: 'output/merged.png'")
+
+if __name__ == '__main__':
+    myPoker = Poker()
+    myPoker.loadFilesName()
+    myPoker.generateImg(300, 100, 2000)
+

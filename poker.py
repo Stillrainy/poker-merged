d19335a8fd5ffd183623951ee5a4f715a50143d5
Shuyuan <sy30@students.uwf.edu>
2020-04-21 00:40:29 -0500

    resize output

--- poker.py
+++ poker.py
@@ -13,15 +13,15 @@ class Poker():
             self.filesName[i] = 'img/' + self.filesName[i]
         return self.filesName
 
-    def generateImg(self, width, height, loop):
+    def generateImg(self, width, height, numOfPokers):
         width = int(width * 94.488188976)
         height = int(height * 94.488188976)
         image = Image.new('RGBA', (width, height))
 
         try:
             from tqdm import tqdm
-            with tqdm(total=loop, ncols=75) as pbar:
-                for i in range(loop):
+            with tqdm(total=numOfPokers, ncols=75) as pbar:
+                for i in range(numOfPokers):
                     curImge = Image.open(
                         self.filesName[random.randint(0, self.length-1)]
                     )
@@ -49,8 +49,8 @@ class Poker():
         image.save('output/merged.png', dpi=(240, 240))
         print(r"Output to: 'output/merged.png'")
 
+
 if __name__ == '__main__':
     myPoker = Poker()
     myPoker.loadFilesName()
-    myPoker.generateImg(300, 100, 2000)
-
+    myPoker.generateImg(147, 100, 5000)
